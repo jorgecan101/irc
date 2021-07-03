563c7d66101c8c94a4674dccaf0b42999022e4b0
jorgecan101 <jorge.cancino101@gmail.com>
2021-07-03 17:51:47 -0500

    adding files that will be used for communication between client and server

---
+++ Client.java
@@ -0,0 +1,13 @@
+import java.util.*; //IDK
+import java.net.*; //For socket things
+import java.io.*; //For IO things
+
+
+public class Client {
+
+
+    public static void main(String args[]) {
+        //stuff for client-side will go here
+    }
+
+}
---
+++ Server.java
@@ -0,0 +1,16 @@
+import java.util.*; //IDK
+import java.net.*; //For socket things
+import java.io.*; //For IO things
+
+public class Server {
+
+    
+
+
+
+
+    public static void main(String args[]) {
+        System.out.println("Hello world");
+    }
+
+}
