c86a607d1ec72c6751afd1dff932d15baa24cab7
jorgecan101 <jorge.cancino101@gmail.com>
2021-07-10 00:01:08 -0500

    couldn't fix it, deal with it later

--- Client.java
+++ Client.java
@@ -6,8 +6,6 @@ public class Client {
         //stuff for client-side will go here
         String hostName = args[0]; //localhost for testing
         int portNum = Integer.parseInt(args[1]); //can be something i.e. 4999
-        
-
         try {
             Socket clientSocket = new Socket(hostName, portNum);
             PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
@@ -15,30 +13,33 @@ public class Client {
             BufferedReader inputBR = new BufferedReader(input);
             String line = inputBR.readLine();
             while (line != null) {
-                System.out.println(line);
+            //    PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
+                output.println(line);
+                //System.out.println(line);
+                output.flush();
                 line = inputBR.readLine();
             }
             inputBR.close();
             output.close();
             clientSocket.close();
-        
-        /*try {
-            //Testing Socket Connection 
-            Socket clientSocket = new Socket(hostName, portNum);
-            //OuputStream to write to the Server
-            PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
-            output.println("hello"); //Client sends hello to the server
-            output.flush(); //flushes stream
-            //InputStreamReader for to read from Server
-            InputStreamReader input = new InputStreamReader(clientSocket.getInputStream());
-            BufferedReader inputBR = new BufferedReader(input);
-
-            String str = inputBR.readLine();
-            System.out.println("Server: " + str); //reads what the server has sent
-        */
         }
         catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
+/*try {
+    //Testing Socket Connection 
+    Socket clientSocket = new Socket(hostName, portNum);
+    //OuputStream to write to the Server
+    PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
+    output.println("hello"); //Client sends hello to the server
+    output.flush(); //flushes stream
+    //InputStreamReader for to read from Server
+    InputStreamReader input = new InputStreamReader(clientSocket.getInputStream());
+    BufferedReader inputBR = new BufferedReader(input);
+
+    String str = inputBR.readLine();
+    System.out.println("Server: " + str); //reads what the server has sent
+*/
--- Server.java
+++ Server.java
@@ -5,34 +5,40 @@ public class Server {
     public static void main(String args[]) {
         int portNum = Integer.parseInt(args[0]); //can be something i.e. 4999
         try {
-
-            
             //Testing Socket Connection 
             ServerSocket serverSocket = new ServerSocket(portNum);
+            System.out.println("Starting Server.");
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client Connected.");
             //InputStream to read from client
             InputStreamReader input = new InputStreamReader(clientSocket.getInputStream());
             BufferedReader inputBR = new BufferedReader(input);
-            String line = "";
-            while (line != null) {
-                line = inputBR.readLine();
-                System.out.println("Client: " + line);
+            //String line = "";
+            String line = inputBR.readLine();
 
+            while (line != null) {
+                //System.out.println("Client: " + line);
+                PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
+                output.println(line);
+                output.flush();
+                line = inputBR.readLine();
             }
             clientSocket.close();
             inputBR.close();
-            /*
-            String str = inputBR.readLine();
-            System.out.println("Client: " + str);
-            //To write to the client
-            PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
-            output.println("yo"); //Server sends to the client
-            output.flush(); //flushes the stream
-            */
+
         }
         catch (IOException e) {
             System.out.println("An Error Has Occured: " + e);
         }
     }
 }
+
+//Ignore rn
+/*
+    String str = inputBR.readLine();
+    System.out.println("Client: " + str);
+    //To write to the client
+    PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
+    output.println("yo"); //Server sends to the client
+    output.flush(); //flushes the stream
+*/
